dfc3ca22fdb77137003e3f8106601af12fc8e132
Amirreza Ipchi-e-Haq <99384891+Amirreza-Ipchi-Haq@users.noreply.github.com>
2023-01-18 20:02:05 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
-#include<conio.h>
+#include<vector>
 #include<iostream>
+unsigned long long n;
 void a(){
-	unsigned long long n;
 	std::cout<<"Please enter the Atomic Number: ",std::cin>>n;
 	if(n==1){
 		std::cout<<"1: 1\n\n";
 		return;
 	}
-	unsigned long long e[n]={0};
-	for(unsigned long long i=0;n;i++)
+	std::vector<unsigned long long>e;
+	for(unsigned long long i=0;n;i++){
+		e.push_back(0);
 		for(unsigned long long j=i/2+i%2,k=(unsigned long long)(i/2)*4+2;j<=i&&n;j++,k-=4)
 			if(n>k)
 				e[j]+=k,n-=k;
 			else
 				e[j]+=n,n=0;
-	for(unsigned long long i=0;e[i];i++){
+	}
+	for(unsigned long long i=0;i<e.size()&&e[i];i++){
 		std::cout<<i+1<<": "<<e[i];
 		if(e[i]==(i+1)*(i+1)*2)
 			std::cout<<" FULL";
